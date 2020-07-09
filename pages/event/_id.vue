8905f6f09b2887b802d6712ca37b628b42cd5f1c
Egor Chebotarev <eg.chebotarev@gmail.com>
2020-07-09 18:35:15 +0300

    Error page & Dynamic router & Nested routers

---
+++ pages/event/_id.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>Event #{{ id }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line
+  name: '_id',
+  computed: {
+    id() {
+      return this.$route.params.id
+    },
+  },
+  head() {
+    return {
+      title: `Event #${this.id}`,
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'You can create Event here',
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<style scoped></style>
