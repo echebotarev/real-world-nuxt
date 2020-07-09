5e5df735ca359404c5c934ec8d30db9982ab1142
Egor Chebotarev <eg.chebotarev@gmail.com>
2020-07-09 19:47:09 +0300

    Работа с API & Обработка недоступного API сервера

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,37 @@
 <template>
   <div>
     <h1>Events</h1>
+
+    <EventCard
+      v-for="(event, index) in events"
+      :key="index"
+      :event="event"
+      :data-index="index"
+    />
   </div>
 </template>
 
 <script>
+import EventCard from '@/components/EventCard'
 export default {
+  components: {
+    EventCard,
+  },
+  asyncData({ $axios, error }) {
+    return $axios
+      .get('http://localhost:8080/events')
+      .then((resp) => {
+        return {
+          events: resp.data,
+        }
+      })
+      .catch((e) => {
+        error({
+          statusCode: 503,
+          message: 'Unable API server',
+        })
+      })
+  },
   head() {
     return {
       title: 'Event Listening',
